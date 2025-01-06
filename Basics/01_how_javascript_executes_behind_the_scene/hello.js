03cb2cc736e17025f4e0ebbff728fa1231cf8e59
divyanshu12341 <divyanshujain111@gmail.com>
2025-01-06 07:01:54 +0530

    Learned data-types constants in Javascript

---
+++ Basics/01_how_javascript_executes_behind_the_scene/hello.js
@@ -0,0 +1,12 @@
+console.log("Hello Chai")
+
+// before code run-time 
+//-------------------------------
+//code file -------> syntax tree 
+//           parse
+//-------------------------------
+
+//JIT compiler is used now a days in javascript
+//JIT compiler creates byte-code that is created just before machine code
+//Then byte code converts it into machine code 
+//When file is converted into byte-code then it is much faster
